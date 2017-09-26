8faf6a320e58bf97347145ec9cd23496ca0b56b1
liuxc_gitlab <liu_xiaochun_ssh@163.com>
2017-09-26 18:23:45 +0800

    add 断路由

--- src/main/java/com/liuxc/proSever/ConsumerServiceApplication.java
+++ src/main/java/com/liuxc/proSever/ConsumerServiceApplication.java
@@ -2,15 +2,21 @@ package com.liuxc.proSever;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@EnableDiscoveryClient     // 注册服务到注册中心
+@EnableDiscoveryClient		// 注册服务到注册中心
 @SpringBootApplication
+@EnableCircuitBreaker		// 开启断路由
 public class ConsumerServiceApplication {
 
+	/**
+	 * RestTemplate 为远程调用模板工具类，方便使用注册中心的服务
+	 * @return
+	 */
 	@Bean
 	@LoadBalanced // 当注册中心同一个服务有多个实例时，可以负载均衡，由ribbon带来该给功能
 	RestTemplate restTemplate() {
--- src/main/java/com/liuxc/proSever/service/ConsumerService.java
+++ src/main/java/com/liuxc/proSever/service/ConsumerService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @Service
 public class ConsumerService {
 
@@ -12,7 +14,16 @@ public class ConsumerService {
 	@Autowired
 	private RestTemplate restTemplate;
 	
+	/**
+	 * 断路由设置，当远程方法异常时， 降级执行fallbackInfo方法
+	 * @return
+	 */
+	@HystrixCommand(fallbackMethod = "fallbackInfo")
 	public String getInfo() {
 		return restTemplate.getForObject("http://" + SERVICE_NAME + "/add?a=10&b=20", String.class);
+	}
+	
+	public String fallbackInfo() {
+		return "fallbackInfo";
 	}
 }
